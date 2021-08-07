719bea33a56fb3d532af84daf4d4244ca8e5760a
Hidetake Iwata <int128@gmail.com>
2021-08-07 15:01:51 +0900

    Paginate list of changed files

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import * as core from '@actions/core'
-import { run } from './run'
+import { parseTransform, run } from './run'
 
 const main = async (): Promise<void> => {
   try {
     const outputs = await run({
       paths: core.getMultilineInput('paths', { required: true }),
       pathsAlways: core.getMultilineInput('paths-always'),
-      transform: core.getMultilineInput('transform', { required: true }),
+      transform: parseTransform(core.getMultilineInput('transform', { required: true })),
       token: core.getInput('token', { required: true }),
     })
     for (const [k, v] of outputs.variables) {
--- src/run.ts
+++ src/run.ts
@@ -1,11 +1,12 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import { PullRequestEvent } from '@octokit/webhooks-types'
 import * as match from './match'
 
 type Inputs = {
   paths: string[]
   pathsAlways: string[]
-  transform: string[]
+  transform: Transform
   token: string
 }
 
@@ -13,45 +14,59 @@ type Outputs = {
   variables: Map<string, string>
 }
 
+type Transform = Map<string, string>
+
 export const run = async (inputs: Inputs): Promise<Outputs> => {
-  if (github.context.eventName !== 'pull_request') {
-    core.info(`Received event ${github.context.eventName} and transform paths to wildcards`)
-    return fallbackToWildcard(inputs)
+  core.info(`eventName: ${github.context.eventName}`)
+  core.info(`transform: ${JSON.stringify([...inputs.transform], undefined, 2)}`)
+
+  if (github.context.eventName === 'pull_request') {
+    return await handlePullRequest(inputs, github.context.payload as PullRequestEvent)
   }
-  return await handlePullRequest(inputs)
+  core.info(`Fallback to wildcards`)
+  return fallbackToWildcard(inputs.transform)
 }
 
-const handlePullRequest = async (inputs: Inputs): Promise<Outputs> => {
-  const octokit = github.getOctokit(inputs.token)
-  //TODO: paginate
-  core.info(`List files in the current pull request`)
-  const { data: listFiles } = await octokit.rest.pulls.listFiles({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    pull_number: github.context.issue.number,
-    per_page: 100,
-  })
+const handlePullRequest = async (inputs: Inputs, e: PullRequestEvent): Promise<Outputs> => {
+  core.info(`${e.pull_request.changed_files} files are changed in the pull request`)
 
+  // limit the max number of changed files to prevent GitHub API rate limit
+  if (e.pull_request.changed_files > 1000) {
+    core.info(`Fallback to wildcards due to too many changed files`)
+    return fallbackToWildcard(inputs.transform)
+  }
+
+  core.info(`List files in the pull request`)
+  const octokit = github.getOctokit(inputs.token)
+  const listFiles = await octokit.paginate(
+    octokit.rest.pulls.listFiles,
+    {
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: github.context.issue.number,
+      per_page: 100,
+    },
+    (r) => r.data
+  )
   const changedFiles = listFiles.map((f) => f.filename)
+  core.info(`Received a list of ${changedFiles.length} files`)
 
   if (match.match(inputs.pathsAlways, changedFiles)) {
     core.info(`paths-always matches to the changed files`)
-    return fallbackToWildcard(inputs)
+    return fallbackToWildcard(inputs.transform)
   }
 
-  core.info(`Transform paths`)
-  const transform = parseTransform(inputs.transform)
+  core.info(`Transform paths by the changed files`)
   const groups = match.exec(inputs.paths, changedFiles)
   const outputVariables = new Map<string, string>()
-  for (const [k, v] of transform) {
+  for (const [k, v] of inputs.transform) {
     const p = match.transform(v, groups)
     outputVariables.set(k, p.join('\n'))
   }
   return { variables: outputVariables }
 }
 
-const fallbackToWildcard = (inputs: Inputs): Outputs => {
-  const transform = parseTransform(inputs.transform)
+const fallbackToWildcard = (transform: Map<string, string>): Outputs => {
   const outputVariables = new Map<string, string>()
   for (const [k, v] of transform) {
     const p = match.transformToWildcard(v)
@@ -60,17 +75,16 @@ const fallbackToWildcard = (inputs: Inputs): Outputs => {
   return { variables: outputVariables }
 }
 
-const parseTransform = (transform: string[]): Map<string, string> => {
+export const parseTransform = (transform: string[]): Transform => {
   const m = new Map<string, string>()
   for (const t of transform) {
     const i = t.indexOf('=')
     if (i < 0) {
-      throw new Error(`transform must be in form of KEY=PATTERN but was ${t}`)
+      throw new Error(`transform must be in form of NAME=PATTERN but was ${t}`)
     }
     const k = t.substring(0, i)
     const v = t.substring(i + 1)
     m.set(k, v)
   }
-  core.info(`Parsed transform as\n${[...m].map(([k, v]) => `${k} => ${v}`).join('\n')}`)
   return m
 }
