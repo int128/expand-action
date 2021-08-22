e5222a25a6db9d04221501dd8e909b5a3b78759f
Hidetake Iwata <int128@gmail.com>
2021-08-22 12:23:37 +0900

    Use eslint:recommended (#24)

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,18 @@ import * as core from '@actions/core'
 import { parseOutputs, run } from './run'
 
 const main = async (): Promise<void> => {
-  try {
-    const outputs = await run({
-      paths: core.getMultilineInput('paths', { required: true }),
-      pathsFallback: core.getMultilineInput('paths-fallback'),
-      outputsMap: parseOutputs(core.getMultilineInput('outputs', { required: true })),
-      token: core.getInput('token', { required: true }),
-    })
-    for (const [k, v] of outputs.map) {
-      core.startGroup(`Set output ${k}`)
-      core.setOutput(k, v)
-      core.info(v)
-      core.endGroup()
-    }
-  } catch (error) {
-    core.setFailed(error.message)
+  const outputs = await run({
+    paths: core.getMultilineInput('paths', { required: true }),
+    pathsFallback: core.getMultilineInput('paths-fallback'),
+    outputsMap: parseOutputs(core.getMultilineInput('outputs', { required: true })),
+    token: core.getInput('token', { required: true }),
+  })
+  for (const [k, v] of outputs.map) {
+    core.startGroup(`Set output ${k}`)
+    core.setOutput(k, v)
+    core.info(v)
+    core.endGroup()
   }
 }
 
-main()
+main().catch((error) => core.setFailed(error))
